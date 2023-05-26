77239a0732b00ca1e7de13d4065e74b72672fec8
TSO <coding.tso@gmail.com>
2023-05-26 18:41:46 +0800

    feat: 4.5

---
+++ chapter4/4.5 嵌套的 effect 与 effect 栈/index.js
@@ -0,0 +1,70 @@
+const bucket = new WeakMap();
+
+const data = { foo: true, bar: true };
+let activeEffect;
+let temp1, temp2;
+
+function effect(fn) {
+    const effectFn = () => {
+        cleanup(effectFn);
+        activeEffect = effectFn;
+        fn();
+    };
+    effectFn.deps = [];
+    effectFn();
+}
+
+function cleanup(effectFn) {
+    for (let i = 0; i < effectFn.deps.length; i++) {
+        const deps = effectFn.deps[i];
+        deps.delete(effectFn);
+    }
+    effectFn.deps.length = 0;
+}
+
+const obj = new Proxy(data, {
+    get(target, key) {
+        track(target, key);
+        return target[key];
+    },
+    set(target, key, newVal) {
+        target[key] = newVal;
+        trigger(target, key);
+    },
+});
+
+effect(function effectFn1() {
+    console.log("effectFn1 执行");
+    effect(function effectFn2() {
+        console.log("effectFn2 执行");
+        temp2 = obj.bar;
+    });
+    temp1 = obj.foo;
+});
+
+setTimeout(() => {
+    obj.foo = false;
+}, 1000);
+
+function track(target, key) {
+    if (!activeEffect) return target[key];
+    let depsMap = bucket.get(target);
+    if (!depsMap) {
+        bucket.set(target, (depsMap = new Map()));
+    }
+    let deps = depsMap.get(key);
+    if (!deps) {
+        depsMap.set(key, (deps = new Set()));
+    }
+    deps.add(activeEffect);
+    activeEffect.deps.push(deps);
+}
+
+function trigger(target, key) {
+    const depsMap = bucket.get(target);
+    if (!depsMap) return;
+    const effects = depsMap.get(key);
+    const effectsToRun = new Set(effects);
+    effectsToRun.forEach((effectFn) => effectFn());
+    // effects && effects.forEach((effect) => effect());
+}

6d54a2673246f78d867dd11b1eb27d82b772b9aa
TSO <coding.tso@gmail.com>
2023-05-27 08:16:32 +0800

    feat: 4.6 避免无限递归循环

---
+++ chapter4/4.6 避免无限递归循环/index.js
@@ -0,0 +1,67 @@
+const bucket = new WeakMap();
+
+const data = { foo: 1 };
+let activeEffect;
+const effectStack = [];
+
+function effect(fn) {
+    const effectFn = () => {
+        cleanup(effectFn);
+        activeEffect = effectFn;
+        effectStack.push(activeEffect);
+        fn();
+        effectStack.pop();
+        activeEffect = effectStack[effectStack.length - 1];
+    };
+    effectFn.deps = [];
+    effectFn();
+}
+
+function cleanup(effectFn) {
+    for (let i = 0; i < effectFn.deps.length; i++) {
+        const deps = effectFn.deps[i];
+        deps.delete(effectFn);
+    }
+    effectFn.deps.length = 0;
+}
+
+const obj = new Proxy(data, {
+    get(target, key) {
+        track(target, key);
+        return target[key];
+    },
+    set(target, key, newVal) {
+        target[key] = newVal;
+        trigger(target, key);
+    },
+});
+
+effect(() => {
+    obj.foo++; // Uncaught RangeError: Maximum call stack size exceeded
+});
+
+setTimeout(() => {
+    obj.foo = false;
+}, 1000);
+
+function track(target, key) {
+    if (!activeEffect) return target[key];
+    let depsMap = bucket.get(target);
+    if (!depsMap) {
+        bucket.set(target, (depsMap = new Map()));
+    }
+    let deps = depsMap.get(key);
+    if (!deps) {
+        depsMap.set(key, (deps = new Set()));
+    }
+    deps.add(activeEffect);
+    activeEffect.deps.push(deps);
+}
+
+function trigger(target, key) {
+    const depsMap = bucket.get(target);
+    if (!depsMap) return;
+    const effects = depsMap.get(key);
+    const effectsToRun = new Set(effects);
+    effectsToRun.forEach((effectFn) => effectFn());
+}
